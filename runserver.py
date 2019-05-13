d2b08d059242176fa4ecb5a350bb1b042ea5d523
navidJadid <m6X2w3iSzaBMx9U0yh2O>
2019-05-13 11:23:22 +0200

    refactor(runserver.py): refactor module for cleaner code

--- runserver.py
+++ runserver.py
@@ -11,15 +11,33 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
+
+def _config_is_debug():
+    # TODO: check if this is better than the above, currently Debug doesn't work when changing SCSS
+    # if 'DEBUG' in app.config and app.config['DEBUG']:
+    #    app.run(host='0.0.0.0', debug=True, threaded=False)
+    return 'DEBUG' in app.config and app.config['DEBUG']
+
+
+def _run_debug_server():
+    print 'Run web server in DEBUG mode'
+    app.run(debug=True)
+
+
+def _run_server():
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    print 'Web server is running. Listening on {}'.format(5000)
+    IOLoop.instance().start()
+
+
 init_app(app, db)
 
 # Start a development web server if executed from the command line
-
 if __name__ == '__main__':
     update_meshes()
-    #if 'DEBUG' in app.config and app.config['DEBUG']:
-    #    app.run(host='0.0.0.0', debug=True, threaded=False)
-    #else:
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5000)
-    IOLoop.instance().start()
+    # app.config['DEBUG'] = True    # comment out if want to run in debug mode
+    if _config_is_debug():
+        _run_debug_server()
+    else:
+        _run_server()
