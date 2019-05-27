dd2be50c2469af43495b65b30e7d406afd29cdf3
navidJadid <m6X2w3iSzaBMx9U0yh2O>
2019-05-27 16:41:21 +0200

    refactor(*): refactor modules in models directories
    (cherry picked from commit b72a38a789b90def0593d8454524f4ac52adac13)

--- webrob/models/experiments.py
+++ webrob/models/experiments.py
@@ -1,18 +1,21 @@
-
 from webrob.app_and_db import db
+
 
 class Project(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
     url = db.Column(db.String(), nullable=False)
 
+
 class Tag(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
 
+
 class Platform(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
+
 
 class Docu(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
