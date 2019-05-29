3e5616cf7aae3e211d96ddf061369c83f7466239
navidJadid <m6X2w3iSzaBMx9U0yh2O>
2019-05-29 16:56:48 +0200

    refactor(login.py): refactor module for better readability
    (cherry picked from commit 198e1520e450241c9a63d95a3aeaffa6187b5601)

--- webrob/pages/login.py
+++ webrob/pages/login.py
@@ -1,4 +1,3 @@
-
 from flask import session, request, redirect, url_for, render_template
 from flask.ext.user.signals import user_logged_in
 from flask.ext.user.signals import user_logged_out
@@ -12,6 +11,7 @@ from string import lowercase
 from webrob.app_and_db import app
 from webrob.docker import docker_interface
 
+
 @user_logged_in.connect_via(app)
 def track_login(sender, user, **extra):
     app.logger.info("Logged in " + str(user.username))
@@ -19,62 +19,82 @@ def track_login(sender, user, **extra):
     session['username'] = user.username
     session['api_token'] = user.api_token
 
+
 @user_logged_out.connect_via(app)
 def track_logout(sender, user, **extra):
     if 'user_container_name' in session:
         docker_interface.stop_container(session['user_container_name'])
         session.pop('user_container_name')
 
+
 @app.route('/')
 def show_user_data():
     if not current_user.is_authenticated:
         return redirect(url_for('user.login'))
-    if not 'user_container_name' in session:
-        ## FIXME: delete knowrob container when user leaves the site
-        #random_name = ''.join(choice(lowercase) for i in range(10))
-        #session['user_container_name'] = random_name
-        #session['username'] = random_name
+    if 'user_container_name' not in session:
+        # return redirect(url_for('user.logout'))
+        # FIXME: delete knowrob container when user leaves the site
+        # _add_random_username_and_container_name_to_session()
         return redirect(url_for('user.logout'))
-    
-    error=""
+
+    error = ""
     # determine hostname/IP we are currently using
     # (needed for accessing container)
     host_url = urlparse(request.host_url).hostname
     container_name = session['user_container_name']
-    role_names = []
-    if hasattr(current_user, 'roles'):
-      role_names = map(lambda x: str(x.name), current_user.roles)
-    
-    category = None
-    exp = None
-    if 'exp-category' in session: category = session['exp-category']
-    if 'exp-name' in session: exp = session['exp-name']
-    
+    role_names = _get_user_roles()
+
+    category = _get_exp_category()
+    exp = _get_exp_name()
+
     # TODO: show_user_data never shown, could be re-enabled as iframe
     return render_template('main.html', **locals())
-    #return render_template('show_user_data.html', **locals())
+    # return render_template('show_user_data.html', **locals())
+
+
+def _add_random_username_and_container_name_to_session():
+    random_name = ''.join(choice(lowercase) for i in range(10))
+    session['user_container_name'] = random_name
+    session['username'] = random_name
+
+
+def _get_user_roles():
+    role_names = []
+    if hasattr(current_user, 'roles'):
+        role_names = map(lambda x: str(x.name), current_user.roles)
+    return role_names
+
+
+def _get_exp_category():
+    if 'exp-category' in session:
+        return session['exp-category']
+    else:
+        return None
+
+
+def _get_exp_name():
+    if 'exp-name' in session:
+        return session['exp-name']
+    else:
+        return None
 
 
 @app.route('/remote')
 def openease_remote():
     if not current_user.is_authenticated:
         return redirect(url_for('user.login'))
-    if not 'user_container_name' in session:
+    if 'user_container_name' not in session:
         return redirect(url_for('user.logout'))
-    
-    error=""
+
+    error = ""
     # determine hostname/IP we are currently using
     # (needed for accessing container)
     host_url = urlparse(request.host_url).hostname
     container_name = 'remote/172.17.42.1'
-    role_names = []
-    if hasattr(current_user, 'roles'):
-      role_names = map(lambda x: str(x.name), current_user.roles)
-    
-    category = None
-    exp = None
-    if 'exp-category' in session: category = session['exp-category']
-    if 'exp-name' in session: exp = session['exp-name']
-    
+    role_names = _get_user_roles()
+
+    category = _get_exp_category()
+    exp = _get_exp_name()
+
     # TODO: show_user_data never shown, could be re-enabled as iframe
     return render_template('main.html', **locals())
