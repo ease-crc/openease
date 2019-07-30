d01eba5282c3b9f099783b102a3b9031c56df582
navidJadid <m6X2w3iSzaBMx9U0yh2O>
2019-07-30 16:39:10 +0200

    test(db_connection_checker_test): add tests for db_connection_checker.py
    tests are rather insufficient though, please consider reading through
    them and improving them
    
    (cherry picked from commit f5eb6afb56e18dba7e8e2c6f7352a918448ad69d)

---
+++ webrob/test/utility/db_connection_checker_test.py
@@ -0,0 +1,54 @@
+import pytest
+
+from webrob.utility.db_connection_checker import got_db_connection
+from webrob.app_and_db import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class MockAppLogger:
+    def __init__(self):
+        return
+
+    def info(self, message):
+        return
+
+
+class MockDbEngineNormal:
+    def __init__(self):
+        self.cmd = "SELECT 1"
+
+    def execute(self, command):
+        # FIXME: Find way to remove hard-coding mock-method
+        if command == self.cmd:
+            return True
+        else:
+            raise Exception
+
+
+class MockDbEngineRaiseError:
+    def __init__(self):
+        return
+
+    def execute(self, command):
+        raise Exception
+
+
+MOCK_LOGGER = MockAppLogger()
+MOCK_DB_ENGINE = MockDbEngineNormal()
+MOCK_DB_ENGINE_MALFUNCTIONING = MockDbEngineRaiseError()
+
+
+# -------------------------------TESTS---------------------------------
+
+
+def test_having_db_connection(monkeypatch):
+    monkeypatch.setattr(Flask, 'logger', MOCK_LOGGER)
+    monkeypatch.setattr(SQLAlchemy, 'engine', MOCK_DB_ENGINE)
+    assert got_db_connection(app, db) is True
+
+
+def test_unable_to_connect_to_db(monkeypatch):
+    monkeypatch.setattr(Flask, 'logger', MOCK_LOGGER)
+    monkeypatch.setattr(SQLAlchemy, 'engine', MOCK_DB_ENGINE_MALFUNCTIONING)
+    assert got_db_connection(app, db) is False
