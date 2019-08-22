3e7afbd3a4af87fc14f3bd8f33b2121071c7e8ce
navidJadid <m6X2w3iSzaBMx9U0yh2O>
2019-08-22 16:44:52 +0200

    test(settings_test.py): add first tests as example
    (cherry picked from commit 0b57f6fdb47b2dbba53c9f91f54ac8638f2f8ce5)

--- webrob/test/config/settings_test.py
+++ webrob/test/config/settings_test.py
@@ -1,3 +1,28 @@
 import pytest
 
 from webrob.config.settings import Config
+
+MAIL_PASSWORD = 'abc'
+
+
+@pytest.fixture
+def monkeypatch_setup(monkeypatch):
+    monkeypatch.setenv('OPENEASE_MAIL_PASSWORD', MAIL_PASSWORD)
+    # TODO: mock all other environment variables, so it does not need to be done twice
+    return monkeypatch
+
+
+# -------------------------------TESTS---------------------------------
+
+# TODO:
+#   add tests for all other environment variables
+
+def test_retrieve_mail_password(monkeypatch_setup):
+    Config._retrieve_mail_password()
+    assert Config.MAIL_PASSWORD == MAIL_PASSWORD
+
+
+def test_retrieve_mail_password_error():
+    with pytest.raises(KeyError):
+        Config._retrieve_mail_password()
+    assert Config.MAIL_PASSWORD is None
