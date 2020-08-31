d516b25362c63b91c3bbb730fb7e320a1e463081
AbhijitVyas <vyasabhijit21@gmail.com>
2020-08-31 11:10:31 +0200

    neem_id type is checked before using it for retrieving neem from mongoDB.

--- webrob/neems/neem.py
+++ webrob/neems/neem.py
@@ -1,43 +1,46 @@
-import os
-import yaml
-from flask import session
 from flask_user import current_user
 
 from webrob.docker.docker_interface import start_user_container
 from webrob.app_and_db import app, mongoDBMetaCollection
 
+import bson
 
 class NEEM:
     def __init__(self,
                  neem_id):
         # collect neem by id
-        if neem_id is None:
-            return None
+        if isinstance(neem_id, unicode):
+            app.logger.info('getting neem from mongo with id: ')
+            app.logger.info(neem_id)
+            b_id = bson.objectid.ObjectId(neem_id)
         else:
-            neem = mongoDBMetaCollection.find_one({"_id": neem_id})
-
-        if neem is None:
-            return None
-        else:
-            self.neem_id = neem['_id']
-            self.name = neem['name']
-            self.description = neem['description']
-            self.created_by = neem['created_by']
-            self.created_at = neem['created_at']
-            self.model_version = neem['model_version']
-            self.downloadUrl = neem['url']
-            self.knowrob_image = 'knowrob'
-            self.knowrob_tag = 'latest'
-            self.maintainer = neem['created_by']
-            self.authors = neem['created_by']
-            self.acknowledgements = ''
-            self.environments = ''
-            self.activities = ''
-            self.agents = ''
+            b_id = neem_id
+        neem = mongoDBMetaCollection.find_one({"_id": b_id})
+        app.logger.info(neem)
+        self.neem_id = str(neem['_id'])
+        app.logger.info("self neemid is ")
+        app.logger.info(type(self.neem_id))
+        # TODO: Tag could be useful for versioning
+        self.neem_tag = ''
+        self.name = neem['name']
+        self.description = neem['description']
+        self.created_by = neem['created_by']
+        self.created_at = neem['created_at']
+        self.model_version = neem['model_version']
+        self.downloadUrl = neem['url']
+        self.knowrob_image = 'knowrob'
+        self.knowrob_tag = 'latest'
+        self.maintainer = neem['created_by']
+        self.authors = neem['created_by']
+        self.acknowledgements = ''
+        self.environments = ''
+        self.activities = ''
+        self.agents = ''
 
     def get_info(self):
         return {
             'neem_id': self.neem_id,
+            'neem_tag': self.neem_tag,
             'name': self.name,
             'description': self.description,
             'maintainer': self.maintainer,
@@ -52,13 +55,16 @@ class NEEM:
         }
 
     def checkout(self):
+        app.logger.info('Checkout neem')
         pass
 
     def activate(self):
-        pass
-        #session['neem_group'] = self.repo_group
-        #session['neem_name'] = self.repo_name  # + ":" + self.repo_tag
-
+        app.logger.info('Activate neem')
+        start_user_container(current_user.username,
+                             self.neem_id,
+                             self.neem_tag,
+                             self.knowrob_image,
+                             self.knowrob_tag)
 
     def matches(self, query_string):
         # TODO
