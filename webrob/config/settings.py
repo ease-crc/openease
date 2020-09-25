a34647983620047978a9c53e599384ab46b1eaa9
Daniel Beßler <danielb@cs.uni-bremen.de>
2020-09-25 10:00:43 +0200

    Update settings.py

--- webrob/config/settings.py
+++ webrob/config/settings.py
@@ -31,4 +31,6 @@ USER_ENABLE_CONFIRM_EMAIL = False
 
 MAX_HISTORY_LINES = 100
 
-MESH_URDF_SERVER = os.environ.get('MESH_URDF_SERVER', 'http://neem-1.informatik.uni-bremen.de/data/')
+MESH_URDF_SERVER = os.environ.get('MESH_URDF_SERVER')
+if bool(MESH_URDF_SERVER)==False:
+    MESH_URDF_SERVER='http://neem-1.informatik.uni-bremen.de/data/'
