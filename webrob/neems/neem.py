53a1ac83fdf625a02eb8505a07154d87c92b4209
AbhijitVyas <vyasabhijit21@gmail.com>
2020-09-25 15:28:32 +0200

    discovery page updated

--- webrob/neems/neem.py
+++ webrob/neems/neem.py
@@ -4,8 +4,9 @@ from webrob.docker.docker_interface import start_user_container
 from webrob.app_and_db import app, mongoDBMetaCollection
 
 from webrob.config.settings import USE_HOST_KNOWROB
-
 import bson
+import json
+from dateutil import parser
 
 class NEEM:
     def __init__(self,
@@ -19,7 +20,7 @@ class NEEM:
         self.neem_id = str(neem['_id'])
         # TODO: Tag could be useful for versioning
         self.neem_tag = ''
-        self.name = neem['name']
+        self.name = 'This is neem name, please test if it works in case of long neem names'
         self.description = neem['description']
         self.created_by = neem['created_by']
         self.created_at = neem['created_at']
@@ -30,11 +31,11 @@ class NEEM:
         self.maintainer = neem['created_by']
         self.authors = neem['created_by']
         self.acknowledgements = ''
-        self.environments = ''
-        self.activities = ''
-        self.agents = ''
+        self.environment = "Kitchen"
+        self.activity = "{'name':'Pizza making', 'url':'https://ease-crc.github.io/soma/owl/current/SOMA.owl'}"
+        self.agent = "Robot"
         self.keywords = neem['keywords']
-        self.image = 'https://blogs.3ds.com/northamerica/wp-content/uploads/sites/4/2019/08/Robots-Square-300x300.jpg'
+        self.image = ''
 
     def get_info(self):
         return {
@@ -45,11 +46,11 @@ class NEEM:
             'maintainer': self.maintainer,
             'authors': self.authors,
             'acknowledgements': self.acknowledgements,
-            'image': 'https://blogs.3ds.com/northamerica/wp-content/uploads/sites/4/2019/08/Robots-Square-300x300.jpg',
+            'image': '',
             'image_tag': self.knowrob_tag,
-            'environments': self.environments,
-            'activities': self.activities,
-            'agents': self.agents,
+            'environment': self.environment,
+            'activity': self.activity,
+            'agent': self.agent,
             'downloadUrl': self.downloadUrl,
             'keywords': self.keywords
         }
