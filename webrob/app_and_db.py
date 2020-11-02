75636b183221266a8d8198c7014c5ce2792a41c9
AbhijitVyas <vyasabhijit21@gmail.com>
2020-11-02 10:35:03 +0100

    links are replaced by buttons in user details page

--- webrob/app_and_db.py
+++ webrob/app_and_db.py
@@ -12,11 +12,17 @@ from flask_sqlalchemy import SQLAlchemy
 import logging
 from pymongo import MongoClient
 
-MONGO_HOST = os.environ.get('KNOWROB_MONGO_HOST')
-MONGO_PORT = int(os.environ.get('KNOWROB_MONGO_PORT'))
-MONGO_DB = os.environ.get('KNOWROB_MONGO_DB')
-MONGO_USER = os.environ.get('KNOWROB_MONGO_USER')
-MONGO_PASS = os.environ.get('KNOWROB_MONGO_PASS')
+# MONGO_HOST = os.environ.get('KNOWROB_MONGO_HOST')
+# MONGO_PORT = int(os.environ.get('KNOWROB_MONGO_PORT'))
+# MONGO_DB = os.environ.get('KNOWROB_MONGO_DB')
+# MONGO_USER = os.environ.get('KNOWROB_MONGO_USER')
+# MONGO_PASS = os.environ.get('KNOWROB_MONGO_PASS')
+
+MONGO_HOST = "mongodb://data.open-ease.org"
+MONGO_PORT = 28015
+MONGO_DB = "neems"
+MONGO_USER = "neemReader"
+MONGO_PASS = "qEWRqc9UdN5TD7No7cjymUA8QEweNz"
 
 # This is the WSGI compliant web application object
 app = Flask(__name__)
