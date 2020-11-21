df61b5b4acfb3e3cb47b99dd5044066e72fa9c6b
Daniel Beßler <danielb@uni-bremen.de>
2020-11-21 20:25:19 +0100

    removed unused dependencies

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 
-global.jQuery = require("jquery"); // "jquery": "2.2.4"
+global.jQuery = require("jquery");
 global.$ = jQuery;
 require('jquery-ui-bundle');
 require('jquery-ui-bundle/jquery-ui.css');
@@ -17,38 +17,6 @@ require('bootstrap/dist/css/bootstrap.css');
 global.fontawesome = require('@fortawesome/fontawesome-free');
 require('@fortawesome/fontawesome-free/js/all.js');
 require('typeface-oswald');
-
-/////////////////////////
-// 
-global.Split = require('split.js')
-
-/////////////////////////
-// overlay loading bar
-global.iosOverlay = require('ios-overlay');
-global.Spinner = require('spin');
-global.createSpinner = function(node) {
-    var parent = node || document.body;
-    var opts = {
-                lines: 13, // The number of lines to draw
-                length: 11, // The length of each line
-                width: 5, // The line thickness
-                radius: 17, // The radius of the inner circle
-                corners: 1, // Corner roundness (0..1)
-                rotate: 0, // The rotation offset
-                color: '#FFF', // #rgb or #rrggbb
-                speed: 1, // Rounds per second
-                trail: 60, // Afterglow percentage
-                shadow: false, // Whether to render a shadow
-                hwaccel: false, // Whether to use hardware acceleration
-                className: 'spinner', // The CSS class to assign to the spinner
-                zIndex: 2e9, // The z-index (defaults to 2000000000)
-                top: 'auto', // Top position relative to parent in px
-                left: 'auto' // Left position relative to parent in px
-    };
-    var target = document.createElement("div");
-    parent.appendChild(target);
-    return new Spinner(opts).spin(target);
-};
 
 /////////////////////////
 // OpenGL
