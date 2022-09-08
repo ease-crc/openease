dbae98d1741ec1bfd658fef405b39543a0c21e46
navidJadid <jadid.navid@yahoo.de>
2022-09-08 02:42:34 +0200

    refactor: clean up runserver.py

--- runserver.py
+++ runserver.py
@@ -29,28 +29,6 @@ USER_ROLES = [
     'editor'
 ]
 
-
-def _config_is_debug():
-    # if environment variable 'EASE_DEBUG' is set to true, then
-    # 'DEBUG' in app.config will be set to true by init_app.py
-    return 'DEBUG' in app.config and app.config['DEBUG']
-
-
-def _run_debug_server():
-    # print 'Run web server in DEBUG mode'
-    # app.run(host='0.0.0.0', debug=True)
-    print 'DEBUG mode currently does not work'
-    print 'Start normal web server'
-    _run_server()
-
-
-def _run_server():
-    http_server = HTTPServer(WSGIContainer(app))
-    http_server.listen(5000)
-    print 'Web server is running. Listening on {}'.format(5000)
-    IOLoop.instance().start()
-
-
 def init_app(extra_config_settings={}):
     # Initialize app config settings
     app.config.from_object('config.settings')  # Read config from 'app/settings.py' file
@@ -105,11 +83,14 @@ def init_app(extra_config_settings={}):
     return app
 
 
-init_app()
 
-# Start a development web server if executed from the command line
+
+def _run_server():
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(5000)
+    IOLoop.instance().start()
+
+
 if __name__ == '__main__':
-    if _config_is_debug():
-        _run_debug_server()
-    else:
-        _run_server()
+    init_app()
+    _run_server()
